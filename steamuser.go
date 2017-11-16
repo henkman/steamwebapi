74ff91590b7b4ee42050d980b7975b10df3c5068
henkman <henkman@github.com>
2017-11-16 22:08:59 +0100

    added GetPlayerSummaries

--- steamuser.go
+++ steamuser.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 type Friend struct {
@@ -33,4 +34,51 @@ func GetFriendList(key, steamid string) ([]Friend, error) {
 		return nil, err
 	}
 	return result.Friendslist.Friends, nil
+}
+
+type PlayerSummary struct {
+	Steamid                  string `json:"steamid"`
+	Communityvisibilitystate int    `json:"communityvisibilitystate"`
+	Profilestate             int    `json:"profilestate"`
+	Personaname              string `json:"personaname"`
+	Lastlogoff               int    `json:"lastlogoff"`
+	Profileurl               string `json:"profileurl"`
+	Avatar                   string `json:"avatar"`
+	Avatarmedium             string `json:"avatarmedium"`
+	Avatarfull               string `json:"avatarfull"`
+	Personastate             int    `json:"personastate"`
+	Realname                 string `json:"realname,omitempty"`
+	Primaryclanid            string `json:"primaryclanid,omitempty"`
+	Timecreated              int    `json:"timecreated,omitempty"`
+	Personastateflags        int    `json:"personastateflags,omitempty"`
+	Gameextrainfo            string `json:"gameextrainfo,omitempty"`
+	Gameid                   string `json:"gameid,omitempty"`
+	Lobbysteamid             string `json:"lobbysteamid,omitempty"`
+	Loccountrycode           string `json:"loccountrycode,omitempty"`
+	Locstatecode             string `json:"locstatecode,omitempty"`
+	Loccityid                int    `json:"loccityid,omitempty"`
+	Commentpermission        int    `json:"commentpermission,omitempty"`
+}
+
+func GetPlayerSummaries(key string, steamids []string) ([]PlayerSummary, error) {
+	const URL = ENDPOINT + "/ISteamUser/GetPlayerSummaries/v2/"
+	ps := url.Values{
+		"key":      []string{key},
+		"steamids": []string{strings.Join(steamids, ",")},
+	}
+	res, err := http.Get(URL + "?" + ps.Encode())
+	if err != nil {
+		return nil, err
+	}
+	var result struct {
+		Response struct {
+			Players []PlayerSummary `json:"players"`
+		} `json:"response"`
+	}
+	err = json.NewDecoder(res.Body).Decode(&result)
+	res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	return result.Response.Players, nil
 }
--- steamuser_test.go
+++ steamuser_test.go
@@ -30,3 +30,28 @@ func TestGetFriendList(t *testing.T) {
 		fmt.Printf("%+v\n", s)
 	}
 }
+
+func TestGetPlayerSummaries(t *testing.T) {
+	key, err := getKey()
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+		return
+	}
+	steamid, err := getSteamId()
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+		return
+	}
+	ss, err := GetPlayerSummaries(key, []string{"76561198009948498",
+		"76561198013516771", steamid})
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+		return
+	}
+	for _, s := range ss {
+		fmt.Printf("%+v\n", s)
+	}
+}
